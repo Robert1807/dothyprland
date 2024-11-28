29c114e2f26c64d5be7179ad3d72f88bd773ae48
Robert1807 <robertrivarola060@gmail.com>
2024-11-28 12:00:23 -0300

    My configs

--- hyprland/config/waybar/wallpaper.sh
+++ hyprland/config/waybar/wallpaper.sh
@@ -38,28 +38,28 @@ case $1 in
         if [ -f $cache_file ]; then
             wal -q -i $current_wallpaper
         else
-            wal -q -i ~/SSD/Pictures/Wallpapers/
+            wal -q -i /home/robert/Imágenes/Wallpapers/
         fi
     ;;
 
     # Seleccionar wallpaper con rofi
     "select")
 
-        selected=$( find "$HOME/SSD/Pictures/Wallpapers/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
+        selected=$( find "$HOME/Imágenes/Wallpapers/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
         do
-            generate_thumbnail "$HOME/SSD/Pictures/Wallpapers/$rfile"  # Generar miniatura
+            generate_thumbnail "$HOME/Imágenes/Wallpapers/$rfile"  # Generar miniatura
             echo -en "$rfile\x00icon\x1f$thumbs_dir/${rfile}.png\n"
         done | rofi -dmenu -replace -disable-history -sort -config ~/.config/rofi/config-wallpaper.rasi)
         if [ ! "$selected" ]; then
             echo "No wallpaper selected"
             exit
         fi
-        wal -q -i ~/SSD/Pictures/Wallpapers/$selected
+        wal -q -i /home/robert/Imágenes/Wallpapers/$selected
     ;;
 
     # Randomly select wallpaper 
     *)
-        wal -q -i ~/SSD/Pictures/Wallpapers/
+        wal -q -i /home/robert/Imágenes/Wallpapers/
     ;;
 
 esac
@@ -96,7 +96,7 @@ sleep 1
 # ----------------------------------------------------- 
 # Obtener nombre del wallpaper y mostrarlo con swww
 # ----------------------------------------------------- 
-newwall=$(echo $wallpaper | sed "s|~/SSD/Pictures/Wallpapers/||g")
+newwall=$(echo $wallpaper | sed "s|/home/robert/Imágenes/Wallpapers/||g")
 
 swww img ~/.cache/current_wallpaper.jpg\
     --transition-bezier .43,1.19,1,.4\
